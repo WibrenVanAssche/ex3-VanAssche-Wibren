5b34abb19d4688482d43f0095d8432f5f790d451
Wibren Van Assche <r0368680@student.thomasmore.be>
2016-10-16 21:47:47 +0200

    niets lukt, alles verwijderd

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 
 Opsplitsen html en js code
 
+hoe data versturen naar 'server'? 
+http://stackoverflow.com/questions/15568851/node-js-how-to-send-data-from-html-to-express
+
+form post rond table zetten in html file
+
+ik heb op internet gezocht hoe ik al mijn js funties kan laten functioneren 'server side' wanneer en bij de client op een knop wordt gedrukt
+==> niets gevonden waar ik iets mee kan doen
+
+niemand in de klas lijkt te weten wat we moeten doen en hoe we moeten beginnen aan het maken van een API
+
+ALLES VERWIJDERD, opnieuw beginnen
+
+
+
--- main.js
+++ main.js
@@ -6,25 +6,5 @@
 var express = require("express");
 var app = new express();
 var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-
-
-function calculate (){
-        answer = eval(command);
-        document.getElementById("scherm").innerHTML = answer;
-        command = answer.toString();
-
-    }
-
-function backslash (){
-        command = command.slice(0,-1);
-        document.getElementById("scherm").innerHTML = command;
-    }
-
-function reset () {
-        location.reload();
-    }
-    
-window.onerror = function(msg, linenumber) {
-    alert('Error message: '+msg);
-    return true;
